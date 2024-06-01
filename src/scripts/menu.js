3736dc1afa6b3356e754f011b5c671bca7799691
Lucidifier <dragan.hardi3@gmail.com>
2024-06-01 03:55:08 +0200

    add code to menu that creates multiple divs for layout

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -6,14 +6,36 @@ function menuPage() {
   const ContainerElement = document.createElement('div');
   const titleElement = document.createElement('div');
   const menuElement = document.createElement('div');
-  
+  let menuDivArr = [];
+
+  //CREATES MULTIPLE DIVS
+  function createMenuDivArray() {
+    for(let i = 0; i <= 7; i++) {
+      menuDivArr[i] = `menuElementDiv${i}`;
+    };
+    return menuDivArr;
+  };
+
+  function createMenuDivs() {
+    for(let i = 0; i <= 7; i++) {
+      menuDivArr[i] = document.createElement('div');
+      contentContainer.appendChild(menuDivArr[i]);
+    };
+  };
 
   function renderMenuPage() {
     clearContent();
-    element.innerText = 'MENU123';
-    contentContainer.appendChild(ContainerElement);
-    console.log('menu');
-    return ;
+    createMenuDivArray()
+    createMenuDivs();
+    menuDivArr[0].innerText = '1';
+    menuDivArr[1].innerText = '2';
+    menuDivArr[2].innerText = '3';
+    menuDivArr[3].innerText = '4';
+    menuDivArr[4].innerText = '5';
+    menuDivArr[5].innerText = '6';
+    menuDivArr[6].innerText = '7';
+    menuDivArr[7].innerText = '8';
+    console.log('menuasd');
   }
   return renderMenuPage();
 }
